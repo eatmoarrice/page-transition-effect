4079caad4c171f68d5aa17ce9b023b961e0d396e
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-10-18 19:41:27 +0700

    added IE support

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const totalPages = 4;
-let navButton = document.getElementById(`navButton`);
-let fakePage1 = document.getElementById('fakePage1');
-let fakePage2 = document.getElementById('fakePage2');
-let menu = document.getElementById('menu');
+'use strict';
 
-const activate = (num) => {
-	let page = document.getElementById(`page-${num}`);
-};
+var totalPages = 4;
+var navButton = document.getElementById('navButton');
+var fakePage1 = document.getElementById('fakePage1');
+var fakePage2 = document.getElementById('fakePage2');
+var menu = document.getElementById('menu');
 
-const showMenu = () => {
+function activate(num) {
+	var page = document.getElementById('page-'.concat(num));
+}
+
+function showMenu() {
 	if (!navButton.classList.contains('open')) {
 		navButton.classList.add('open');
-		for (let i = 1; i <= totalPages; i++) {
-			let page = document.getElementById(`page-${i}`);
+
+		for (var i = 1; i <= totalPages; i++) {
+			var page = document.getElementById('page-'.concat(i));
+
 			if (page.classList.contains('active')) {
 				page.classList.add('page--transform');
 				page.classList.remove('active');
@@ -21,58 +25,71 @@ const showMenu = () => {
 				page.classList.add('page--standby');
 			}
 		}
+
 		showFakePages();
 
-		for (let i = 1; i <= totalPages; i++) {
-			let menu = document.getElementById(`menu__${i}`);
+		var _loop = function _loop(_i) {
+			var menu = document.getElementById('menu__'.concat(_i));
 			setTimeout(function () {
 				menu.classList.add('fade-in');
-			}, 200 * i);
+			}, 200 * _i);
+		};
+
+		for (var _i = 1; _i <= totalPages; _i++) {
+			_loop(_i);
 		}
 	} else {
 		closeMenu();
 	}
-};
+}
 
-const showFakePages = () => {
+function showFakePages() {
 	fakePage1.classList.add('fakePage--page1');
 	fakePage2.classList.add('fakePage--page2');
-};
+}
 
-const choosePage = (num) => {
-	let page = document.getElementById(`page-${num}`);
+function choosePage(num) {
+	var page = document.getElementById('page-'.concat(num));
 	page.classList.add('active');
 	page.classList.add('page--appear');
 	page.classList.remove('page--hidden');
 	page.classList.remove('page--standby');
 	setTimeout(function () {
-		for (let i = 1; i <= totalPages; i++) {
-			let page = document.getElementById(`page-${i}`);
-			page.classList.remove('semi-active');
+		for (var i = 1; i <= totalPages; i++) {
+			var _page = document.getElementById('page-'.concat(i));
+
+			_page.classList.remove('semi-active');
 		}
+
 		closeMenu();
 	}, 500);
-};
+}
 
-const closeMenu = () => {
+function closeMenu() {
 	navButton.classList.remove('open');
-	for (let i = 1; i <= totalPages; i++) {
-		let page = document.getElementById(`page-${i}`);
+
+	for (var i = 1; i <= totalPages; i++) {
+		var page = document.getElementById('page-'.concat(i));
+
 		if (page.classList.contains('semi-active')) {
 			page.classList.remove('semi-active');
 			page.classList.add('active');
 		}
+
 		page.classList.remove('page--transform');
 		page.classList.remove('page--appear');
+
 		if (!page.classList.contains('active')) {
 			page.classList.add('page--hidden');
 		}
 	}
+
 	fakePage1.classList.remove('fakePage--page1');
 	fakePage2.classList.remove('fakePage--page2');
 
-	for (let i = 1; i <= totalPages; i++) {
-		let menu = document.getElementById(`menu__${i}`);
-		menu.classList.remove('fade-in');
+	for (var _i2 = 1; _i2 <= totalPages; _i2++) {
+		var _menu = document.getElementById('menu__'.concat(_i2));
+
+		_menu.classList.remove('fade-in');
 	}
-};
+}
